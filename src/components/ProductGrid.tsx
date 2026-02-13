ed9e0240c654236b2533e689fa5eff8026d38d5a
rahmat-bagus-santoso <bagussantoso4146@gmail.com>
2026-02-13 14:44:40 +0800

    fix:card UI

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -10,11 +10,8 @@ const ProductGrid = ({ products }: ProductGridProps) => {
   return (
     <div className="grid grid-cols-1 gap-y-14 gap-x-8 sm:grid-cols-2 lg:grid-cols-3">
       {products.map((product) => (
-        <Link key={product.id} href={`/product/${product.id}`} className="block transition-transform hover:-translate-y-1">
-          <ProductCard product={product} />
-        </Link>
+          <ProductCard key={product.id} product={product} />
       ))}
-      
     </div>
   )
 }
